6bf84b55ed2ae57fa4ae7db126f1a3b15851ecf3
Lee Martin <lee.martin@pliminus.co.uk>
2021-06-06 14:09:46 +0000

    Added: Constants for HttpClient names

---
+++ Properties/HttpClientNames.cs
@@ -0,0 +1,7 @@
+namespace PaceMe.BlazorApp.Properties
+{
+    public static class HttpClientNames {
+        public static string FunctionApi = "FunctionApi";
+        public static string FunctionApiAnonymous = "FunctionApiAnonymous";
+    }
+}
--- Properties/PaceMeFunctionAppConfiguration.cs
+++ Properties/PaceMeFunctionAppConfiguration.cs
@@ -1,6 +1,6 @@
 namespace PaceMe.BlazorApp.Properties
 {
-    public class PaceMeFunctionAppConfiguration {
+    public static class PaceMeFunctionAppConfiguration {
         public static string PaceMeFunctionAppUrl = "PaceMeFunctionAppUrl";
         public static string PaceMeFunctionAppAccessScope = "PaceMeFunctionAppAccessScope";
     }
